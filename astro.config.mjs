e2d91aa8904780afce4adf50e6e53435e947064c
Juan Carlos Duran <qheolet@gmail.com>
2024-01-21 22:40:01 -0500

    Update astro.config.mjs
    add changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	  site: 'https://qheolet.github.io',
+  base: '/zapping-zero',
 	integrations: [mdx(), sitemap()],
 });
