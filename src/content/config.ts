e253a91f0a53c2e78862e77c18e8f07355081c32
Juan Carlos Duran <juand.vendorn@herbalife.com>
2024-01-21 22:21:59 -0500

    init 2.0

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,7 +10,23 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		draft: z.boolean().optional()
 	}),
 });
 
-export const collections = { blog };
+const page = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		draft: z.boolean().optional()
+	}),
+});
+
+
+export const collections = { blog, page };
