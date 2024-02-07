e035b250c2786b7be64f55652998548d87d98670
Juan Carlos Duran <juand.vendorn@herbalife.com>
2024-02-07 08:54:36 -0500

    add changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
-import { remarkReadingTime } from './src/utils/readTime.ts'
+import sitemap from "@astrojs/sitemap";
+import { remarkReadingTime } from "./src/utils/readTime.ts";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astro-cactus.chriswilliams.dev/",
-	integrations: [mdx(), sitemap()],
-	markdown:{
-		remarkPlugins:[ remarkReadingTime ]
-	}
+  site: "https://localhost:4502/",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,33 +1,31 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		draft: z.boolean().optional(),
-		tags: z.string().array().optional()
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    draft: z.boolean().optional(),
+    // tags: z.string().array().optional(),
+  }),
 });
 
 const page = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    // description: z.string(),
+    // Transform string to Date object
+    // pubDate: z.coerce.date(),
+    // updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
-
-
-export const collections = { blog,page };
+export const collections = { blog, page };
